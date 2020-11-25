4904e661e4f81626271a8831921c6ea8e7b2209d
Diego Davila <davila.foyo@gmail.com>
2020-11-25 10:59:15 -0800

    use frontend.xml.base instead of frontend.xml.k8s, removed 05_test.sh and added docker documentation

--- frontend/image-config.d/10_change_own.sh
+++ frontend/image-config.d/10_change_own.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp /etc/gwms-frontend/frontend.xml.k8s /etc/gwms-frontend/frontend.xml 
+cp /etc/gwms-frontend/frontend.xml.base /etc/gwms-frontend/frontend.xml 
 chown frontend /etc/gwms-frontend/frontend.xml
