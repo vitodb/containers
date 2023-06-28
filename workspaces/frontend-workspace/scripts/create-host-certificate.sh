5e1d3b7756799ccfadb3c7b50718436c2356875d
Bruno Coimbra <coimbra@fnal.gov>
2023-06-28 15:39:27 -0500

    Add gwms-workspace-ca

--- workspaces/frontend-workspace/scripts/create-host-certificate.sh
+++ workspaces/frontend-workspace/scripts/create-host-certificate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Set the file paths and details for the certificate
+CA_CERT="/opt/GlideinWMS/secrets/gwms-workspace-ca-cert.pem"
+CA_KEY="/opt/GlideinWMS/secrets/gwms-workspace-ca-key.pem"
 KEY_FILE="/etc/grid-security/hostkey.pem"
 CERT_FILE="/etc/grid-security/hostcert.pem"
 PASSPHRASE="glideinwms"
@@ -11,10 +13,10 @@ VALIDITY_PERIOD=365
 openssl genpkey -algorithm RSA -out "$KEY_FILE" -pass pass:"$PASSPHRASE"
 
 # Generate a certificate signing request (CSR)
-openssl req -new -key "$KEY_FILE" -out "$CERT_FILE" -subj "/CN=$COMMON_NAME"
+openssl req -new -key "$KEY_FILE" -out "$CERT_FILE" -subj "/C=US/ST=Illinois/L=Batavia/O=Fermilab/CN=$COMMON_NAME"
 
 # Generate a self-signed certificate using the CSR
-openssl x509 -req -in "$CERT_FILE" -signkey "$KEY_FILE" -out "$CERT_FILE" -days "$VALIDITY_PERIOD"
+openssl x509 -req -in "$CERT_FILE" -CA "$CA_CERT" -CAkey "$CA_KEY" -out "$CERT_FILE" -days "$VALIDITY_PERIOD"
 
-cp $CERT_FILE /etc/pki/ca-trust/source/anchors/
+cp "$CA_CERT" /etc/pki/ca-trust/source/anchors/
 update-ca-trust extract
